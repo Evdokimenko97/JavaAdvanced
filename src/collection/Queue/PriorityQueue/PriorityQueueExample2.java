7eeab475a9b250c7706162d0330cd6ff373a52e5
Andrew Evdokimenko <evdokimenko_9797@mail.ru>
2024-07-12 16:35:23 +0300

    Queue and Deque

---
+++ src/collection/Queue/PriorityQueue/PriorityQueueExample2.java
@@ -0,0 +1,63 @@
+package collection.Queue.PriorityQueue;
+
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+public class PriorityQueueExample2 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Zaur", 3);
+        Student st2 = new Student("Masha", 1);
+        Student st3 = new Student("Dasha", 5);
+        Student st4 = new Student("Oleg", 2);
+        Student st5 = new Student("Igor", 5);
+
+        PriorityQueue<Student> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(st1);
+        priorityQueue.add(st2);
+        priorityQueue.add(st3);
+        priorityQueue.add(st4);
+        priorityQueue.add(st5);
+        System.out.println(priorityQueue);
+
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+    }
+}
+
+class Student implements Comparable<Student> {
+    String name;
+    int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.course - student.course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course);
+    }
+}
